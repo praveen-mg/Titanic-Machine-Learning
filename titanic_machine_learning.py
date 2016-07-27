fb487b79f574dc4ea88c50091447c62bc9207bf0
PRAVEEN KUMAR MEENAKSHI SUNDARAM GANDHI <PRAVEEN KUMAR MEENAKSHI SUNDARAM GANDHI>
2016-07-27 20:56:52 +0800

    data cleaned

--- titanic_machine_learning.py
+++ titanic_machine_learning.py
@@ -5,3 +5,27 @@ Created on Wed Jul 27 17:23:29 2016
 @author: praveen
 """
 
+import pandas
+
+titanic = pandas.read_csv("train.csv")
+
+print(titanic.head(5))
+print (titanic.describe())
+
+titanic["Age"] = titanic["Age"].fillna(titanic["Age"].median())
+
+print (titanic["Sex"].unique())
+
+titanic.loc[titanic["Sex"] == "male","Sex"] = 0
+titanic.loc[titanic["Sex"] == "female","Sex"] = 1
+
+print (titanic["Sex"].unique())
+
+titanic["Embarked"] = titanic["Embarked"].fillna("S")
+
+print titanic["Embarked"].unique()
+
+titanic.loc[titanic["Embarked"] == "S","Embarked"] = 0
+titanic.loc[titanic["Embarked"] == "C","Embarked"] = 1
+titanic.loc[titanic["Embarked"] == "Q", "Embarked"] = 2
+
